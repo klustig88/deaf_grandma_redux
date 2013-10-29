fc30fd8aa253c1fdccb2a74ecfd9e150f487414f
Kevin Lustig <kevin.lustig88@gmail.com>
2013-10-29 10:48:27 -0500

    Update index.rb

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,26 +1,18 @@
 get '/' do
-  
-if params[:grandma] == params[:grandma].upcase
-  @grandma = "Thats nice!"
-
+  if params[:grandma] == params[:grandma].upcase
+    @grandma = "Thats nice!"
   erb :index  
 else
   @grandma = "Speak up, Sonny!"
   # Look in app/views/index.erb
   erb :index         # respond by showing index.erb  / template rendering 
-end
+  end
 end 
 
-# get '/:word' do 
-	# puts params[:word]
-# end 
+
 
 post '/grandma' do
-	# puts request.body.read
-	# puts params.inspect
-	p params[:user_input]
-  # "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
-  # redirect back to resource 
+  p params[:user_input]
   redirect("/?grandma=#{params[:user_input]}")
 end
 
